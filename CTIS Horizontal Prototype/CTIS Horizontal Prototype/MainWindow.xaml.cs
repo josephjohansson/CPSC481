7bf56b8301130da5fc8e3f49f89e077b5f05d675
Jose <rodrigjg@ucalgary.ca>
2016-03-14 03:37:38 -0600

    Expanded the scope of the prototype greatly added readme

--- CTIS Horizontal Prototype/CTIS Horizontal Prototype/MainWindow.xaml.cs
+++ CTIS Horizontal Prototype/CTIS Horizontal Prototype/MainWindow.xaml.cs
@@ -31,78 +31,218 @@ namespace CTIS_Horizontal_Prototype
             Swipe_Card_Button.AddHandler(Button.MouseDownEvent, new RoutedEventHandler(Swipe_Card_Button_MouseDown), true);
             cardMode=0;
         }
-
+        //top left button
         private void blank_metal_button_croppoed_jpg_MouseDown(object sender, MouseButtonEventArgs e)
         {
-
+            //If its on the main screen selecting the button will take you to the choose payment screen
             if (MainGrid.Visibility == Visibility.Visible)
             {
                 MainGrid.Visibility = Visibility.Collapsed;
-                CoinGrid.Visibility = Visibility.Visible;
+                ChoosePaymentGrid.Visibility = Visibility.Visible;
+                Swipe_Card_Button.Visibility = Visibility.Collapsed;
+                cardMode = 0;
             }
+            //Selecting Credit
+            else if (ChoosePaymentGrid.Visibility == Visibility.Visible)
+            {
+                ChoosePaymentGrid.Visibility = Visibility.Collapsed;
+                DebsCredsGrid.Visibility = Visibility.Visible;
+            }
+            //Printing Ticket or tapping card
+            else if (DebsCredsGrid.Visibility == Visibility.Visible)
+            {
+                DebsCredsGrid.Visibility = Visibility.Collapsed;
+                Swipe_Card_Button.Visibility = Visibility.Visible;
+                TapOrPrint.Visibility = Visibility.Visible;
+            }
+            //Go to bought ticket screen
+            else if (TapOrPrint.Visibility == Visibility.Visible)
+            {
+                TapOrPrint.Visibility = Visibility.Collapsed;
+                Swipe_Card_Button.Visibility = Visibility.Collapsed;
+                BuyGrid.Visibility = Visibility.Visible;
+            }
+            //go to individual bus route screen
+            else if (BussesGrid.Visibility == Visibility.Visible)
+            {
+                BussesGrid.Visibility = Visibility.Collapsed;
+                BusRoutGrid.Visibility = Visibility.Visible;
+            }
+
 
             if (SettingsGrid.Visibility == Visibility.Visible)
             {
-                SettingsGrid.Visibility = Visibility.Collapsed;
-                SettingsConfirmGrid.Visibility = Visibility.Visible;
-                if (sender.Equals(blank_metal_button_croppoed_jpg))
+                
+                if (sender.Equals(blank_metal_button_croppoed_jpg) && Swipe_Card_Button.Visibility == Visibility.Collapsed)
                 {
                     cardMode = 0;
                     buymessage.Content = "You have\npurchased a\nsingle ticket!";
+                    SettingsGrid.Visibility = Visibility.Collapsed;
+                    SettingsConfirmGrid.Visibility = Visibility.Visible;
                 }
-                else if (sender.Equals(blank_metal_button_croppoed_jpg_Copy))
+                else if (sender.Equals(blank_metal_button_croppoed_jpg_Copy) && Swipe_Card_Button.Visibility == Visibility.Collapsed)
                 {
                     cardMode = 1;
                     buymessage.Content = "You have\npurchased a\nday pass!";
+                    SettingsGrid.Visibility = Visibility.Collapsed;
+                    SettingsConfirmGrid.Visibility = Visibility.Visible;
                 }
-                else if (sender.Equals(blank_metal_button_croppoed_jpg_Copy4))
+                else if (sender.Equals(blank_metal_button_croppoed_jpg_Copy4) && Swipe_Card_Button.Visibility == Visibility.Collapsed)
                 {
                     cardMode = 2;
                     buymessage.Content = "You have\npurchased a\nmonthly pass!";
+                    SettingsGrid.Visibility = Visibility.Collapsed;
+                    SettingsConfirmGrid.Visibility = Visibility.Visible;
                 }
+                else
+                {
+                    setlabel_Copy2.Visibility = Visibility.Visible;
+                }
+                
             }
 
 
         }
-
+        //Cancel Button
         private void cancel_button_MouseDown(object sender, MouseButtonEventArgs e)
         {
-            if (!(MainGrid.Visibility == Visibility.Visible))
+            //go back from choosing a ticket type in main
+            if (ChoosePaymentGrid.Visibility == Visibility.Visible)
             {
-                CoinGrid.Visibility = Visibility.Collapsed;
-                BuyGrid.Visibility = Visibility.Collapsed;
-                MapGrid.Visibility = Visibility.Collapsed;
+                ChoosePaymentGrid.Visibility = Visibility.Collapsed;
+                MainGrid.Visibility = Visibility.Visible;
+                Swipe_Card_Button.Visibility = Visibility.Visible;
+            }
+            //Go back from choosing credit
+            else if (DebsCredsGrid.Visibility == Visibility.Visible)
+            {
+                DebsCredsGrid.Visibility = Visibility.Collapsed;
+                ChoosePaymentGrid.Visibility = Visibility.Visible;
+            }
+            //Go back from settings
+            else if (SettingsGrid.Visibility == Visibility.Visible)
+            {
                 SettingsGrid.Visibility = Visibility.Collapsed;
+                MainGrid.Visibility = Visibility.Visible;
+                Swipe_Card_Button.Visibility = Visibility.Visible;
+            }
+            //Go back form confirmed settings
+            else if (SettingsConfirmGrid.Visibility == Visibility.Visible)
+            {
                 SettingsConfirmGrid.Visibility = Visibility.Collapsed;
                 MainGrid.Visibility = Visibility.Visible;
+                Swipe_Card_Button.Visibility = Visibility.Visible;
             }
-        }
+            //Go back from paid ticket
+            else if (BuyGrid.Visibility == Visibility.Visible)
+            {
+                BuyGrid.Visibility = Visibility.Collapsed;
+                MainGrid.Visibility = Visibility.Visible;
+                Swipe_Card_Button.Visibility = Visibility.Visible;
+            }
+            //Go back from bus route map
+            else if (BusRoutGrid.Visibility == Visibility.Visible)
+            {
+                BusRoutGrid.Visibility = Visibility.Collapsed;
+                BussesGrid.Visibility = Visibility.Visible;
+            }
+            //Go back from bus selection screen
+            else if (BussesGrid.Visibility == Visibility.Visible)
+            {
+                BussesGrid.Visibility = Visibility.Collapsed;
+                MainGrid.Visibility = Visibility.Visible;
+                Swipe_Card_Button.Visibility = Visibility.Visible;
+            }
+            //go back from train map
+            else if (MapGrid.Visibility == Visibility.Visible)
+            {
+                MapGrid.Visibility = Visibility.Collapsed;
+                MainGrid.Visibility = Visibility.Visible;
+                Swipe_Card_Button.Visibility = Visibility.Visible;
+            }
+            
 
+        }
+        //Train Map
         private void blank_metal_button_croppoed_jpg_Copy12_MouseDown(object sender, MouseButtonEventArgs e)
         {
             if (MainGrid.Visibility == Visibility.Visible)
             {
                 MainGrid.Visibility = Visibility.Collapsed;
                 MapGrid.Visibility = Visibility.Visible;
+                Swipe_Card_Button.Visibility = Visibility.Collapsed;
             }
         }
-
+        //Quickpay with card
         private void Swipe_Card_Button_MouseDown(object sender, RoutedEventArgs e)
         {
-            MainGrid.Visibility = Visibility.Collapsed;
-            SettingsGrid.Visibility = Visibility.Collapsed;
-            MapGrid.Visibility = Visibility.Collapsed;
-            CoinGrid.Visibility = Visibility.Collapsed;
-            SettingsConfirmGrid.Visibility = Visibility.Collapsed;
-            BuyGrid.Visibility = Visibility.Visible;
+            if (Swipe_Card_Button.Visibility == Visibility.Visible && SettingsGrid.Visibility == Visibility.Visible)
+            {
+                Swipe_Card_Button.Visibility = Visibility.Collapsed;
+                setlabel_Copy2.Visibility = Visibility.Collapsed;
+                setlabel_Copy3.Visibility = Visibility.Visible;
+            }
+            
+            else
+            {
+                MainGrid.Visibility = Visibility.Collapsed;
+                SettingsGrid.Visibility = Visibility.Collapsed;
+                MapGrid.Visibility = Visibility.Collapsed;
+                CoinGrid.Visibility = Visibility.Collapsed;
+                SettingsConfirmGrid.Visibility = Visibility.Collapsed;
+                BuyGrid.Visibility = Visibility.Visible;
+                TapOrPrint.Visibility = Visibility.Collapsed;
+                Swipe_Card_Button.Visibility = Visibility.Collapsed;
+            }
+            
         }
 
+        //Pull up settings
         private void blank_metal_button_croppoed_jpg_Copy11_MouseDown(object sender, MouseButtonEventArgs e)
         {
             if (MainGrid.Visibility == Visibility.Visible)
             {
                 MainGrid.Visibility = Visibility.Collapsed;
                 SettingsGrid.Visibility = Visibility.Visible;
+            }
+            else if (ChoosePaymentGrid.Visibility == Visibility.Visible)
+            {
+                ChoosePaymentGrid.Visibility = Visibility.Collapsed;
+                DebsCredsGrid.Visibility = Visibility.Visible;
+            }
+        }
+        //Bus 
+        private void blank_metal_button_croppoed_jpg_Copy16_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (MainGrid.Visibility == Visibility.Visible)
+            {
+                MainGrid.Visibility = Visibility.Collapsed;
+                BussesGrid.Visibility = Visibility.Visible;
+                Swipe_Card_Button.Visibility = Visibility.Collapsed;
+            }
+            else if(ChoosePaymentGrid.Visibility == Visibility.Visible)
+            {
+                ChoosePaymentGrid.Visibility = Visibility.Collapsed;
+                CoinGrid.Visibility = Visibility.Visible;
+                Swipe_Card_Button.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void Rectangle_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (CoinGrid.Visibility == Visibility.Visible)
+            {
+                CoinGrid.Visibility = Visibility.Collapsed;
+                BuyGrid.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void label3_Copy4_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (DebsCredsGrid.Visibility == Visibility.Visible)
+            {
+                DebsCredsGrid.Visibility = Visibility.Collapsed;
+                BuyGrid.Visibility = Visibility.Visible;
             }
         }
     }
---
+++ CTIS Horizontal Prototype/CTIS Horizontal Prototype/README.txt
@@ -0,0 +1,40 @@
+Team: CTIS(Calgary Transit Information Systems)
+Memebers: Jose Gabriel Rodriguez Gumucio
+		  David Wilkinson
+How to use:
+
+To buy a ticket if you are not a transit card holder:
+1. Press any button on the left(buttons are the metalic looking round squares). Select method of payment in teh same way.
+2a. For debit and credit you can click on the text that says "tap your debit/credit car" or click the Use Chip/Swipe button.
+2b. For Cash pres the red square to proceed
+3. Choose to tap your transit card to "reload it" or press the print ticket button.
+4. Press Cancel to return to the main screen
+
+To Buy a pass with the preset transit card settings:
+1. Pretend to tap transit card by clicking the tap transit card button.
+
+
+To change what ticket is automatically bought or to check time remaining in card:
+
+1. Press QuickCard Settings
+2. Pretened to Tap transit card by clicking on the button
+3. select and option or view remaining time(note time does not change).
+4. Press Cancel to get back to the main screen. 
+5. Buy a ticket again notice the difference in the alert after buying the ticket. 
+Note: prepaid credit cards wouldhave to be set up using the city website. 
+
+
+To check train map and locations:
+
+1. Press Train Locations
+2. Press cancel to get back to the main screen
+
+To check nearby bus times:
+1. Press Nearby bus times
+2. choose route
+3. check bus times and bus route. 
+4. press cancel twice to get back to main screen. 
+Note: Right side routes and "More..." DOnt work. 
+
+Note:
+Help button does not work.
